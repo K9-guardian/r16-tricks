eadef4bd6ef36184c654fadcaa176a42a325642a
K9-guardian <girkar.km@gmail.com>
2024-01-04 17:14:58 -0800

    use edit distance and filter stuff shorter than the search

--- lexicon/lexicon.rkt
+++ lexicon/lexicon.rkt
@@ -6,42 +6,48 @@
     [(libdata-lexicon) entries]
     [(libsplit-text*) (λ (x _) (list x))]))
 (define (make-attachment payload _name _mime) payload)
-(define string-args "tater")
+(define string-args "spectrolus")
 
 (define lexicon-base "https://botaniamod.net/lexicon.html#")
-(define custom-inputs
-  '(("tater" . "Tiny Potato")
-    ("pew pew" . "Rod of the Unstable Reservoir")))
 
-(define (num-matches title args)
-  (define args*
-    (or (and~>> custom-inputs
-                (assf (curry string-contains? (string-foldcase args)))
-                cdr
-                string-foldcase)
-        (string-foldcase args)))
-  (define title* (string-foldcase title))
-  (define (smart-string-prefix? str1 str2)
-    (define lst (list str1 str2))
-    (>= (~>> lst (map string->list) (apply take-common-prefix) length)
-        (~>> lst (map string-length) (apply min) (max 3))))
-  (for*/sum ([title-str (string-split title*)]
-             [arg-str (string-split args*)]
-             #:when (smart-string-prefix? title-str arg-str))
-    (~>> (list title-str arg-str)
-         (map string->list)
-         (apply take-common-prefix)
-         length)))
+(define (edit-distance str1 str2)
+  (define rows (add1 (string-length str1)))
+  (define cols (add1 (string-length str2)))
+  (define dp (make-vector (* rows cols) 0))
+  (define (coord r c) (+ (* r cols) c))
+  (for ([i (range 1 rows)]) (vector-set! dp (coord i 0) i))
+  (for ([j (range 1 cols)]) (vector-set! dp (coord 0 j) j))
+  (for* ([j (range 1 cols)] [i (range 1 rows)])
+    (define substitution-cost (if (eq? (string-ref str1 (sub1 i)) (string-ref str2 (sub1 j))) 0 1))
+    (vector-set! dp
+                 (coord i j)
+                 (min (add1 (vector-ref dp (coord (sub1 i) j)))
+                      (add1 (vector-ref dp (coord i (sub1 j))))
+                      (+ (vector-ref dp (coord (sub1 i) (sub1 j))) substitution-cost))))
+  (vector-ref dp (coord (sub1 rows) (sub1 cols))))
+
+(define (distance title arg)
+  (define title-words (string-split (string-foldcase title)))
+  (define arg-words (string-split (string-foldcase arg)))
+  (for/sum ([arg-str arg-words])
+    (~>> (for/list ([title-str title-words]
+                    #:when (<= (string-length arg-str) (string-length title-str)))
+           (edit-distance arg-str (substring title-str 0 (string-length arg-str))))
+         (cons +inf.0)
+         (apply min))))
 
 (~>> (call-trick 'libdata-lexicon #f)
-     (map (λ (entry)
-            (cons
-             (num-matches (car entry) string-args)
-             (cdr entry))))
-     (argmax car)
+     (filter (λ~>> car string-split length (<= (~> string-args string-split length))))
+     (map (λ (entry) (cons (distance (car entry) string-args) entry)))
+     (sort _ (λ (p1 p2)
+               (cond
+                 [(< (car p1) (car p2)) #t]
+                 [(> (car p1) (car p2)) #f]
+                 [else (< (cdr p1) (cdr p2))]))
+           #:key (λ (item) (cons (car item) (string-length (cadr item)))))
+     car
      (match _
-       [(cons 0 _) "No matches were found!"]
-       [(list _ bookmark text)
+       [(list _ _ bookmark text)
         (values
          (~a lexicon-base bookmark)
          (make-attachment
